e65733e12979e0b04f6f8b9f6c1fa25f18380d8a
reshetnikd <dmitry.reshetnik@icloud.com>
2021-04-07 19:39:33 +0300

    Change the background

--- Light/ViewController.swift
+++ Light/ViewController.swift
@@ -8,8 +8,19 @@
 import UIKit
 
 class ViewController: UIViewController {
+    var lightOn = true
 
+    fileprivate func updateUI() {
+        if lightOn {
+            view.backgroundColor = .white
+        } else {
+            view.backgroundColor = .black
+        }
+    }
+    
     @IBAction func buttonPressed(_ sender: Any) {
+        lightOn.toggle()
+        updateUI()
     }
     
     override func viewDidLoad() {
