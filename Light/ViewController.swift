aa8b2cf7e2d358dffc1f992fdf28abeee2df0331
reshetnikd <dmitry.reshetnik@icloud.com>
2021-04-07 19:34:35 +0300

    Create a button and an action

--- Light/ViewController.swift
+++ Light/ViewController.swift
@@ -9,6 +9,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBAction func buttonPressed(_ sender: Any) {
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
